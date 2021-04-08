835f14cfa1feff09fcf5152397171f38b17fc26d
ImperadorPombinho <pedrobarros0101@outlook.com>
2021-04-08 00:09:41 -0300

    primeira classe: posicao

--- aplicacao/mainzada.java
+++ aplicacao/mainzada.java
@@ -1,8 +1,11 @@
 package Xadrez.aplicacao;
 
+import Xadrez.tabuleirogame.posicao;
+
 public class mainzada {
     public static void main(String[] args) {
-        System.out.println("ola");
+        posicao pos = new posicao(3, 5);
+        System.out.println(pos);
     }
     
 }
---
+++ tabuleirogame/posicao.java
@@ -0,0 +1,33 @@
+package Xadrez.tabuleirogame;
+
+public class posicao {
+    private int linha;
+    private int coluna;
+    
+    public posicao(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+    //METODO:
+    @Override
+    public String toString(){
+        return getLinha() + ", " + getColuna();
+    }
+}
