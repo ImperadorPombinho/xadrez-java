7c4a32449c786305c67e1cdeb07939e68538ef64
ImperadorPombinho <pedrobarros0101@outlook.com>
2021-04-09 00:07:39 -0300

    limpando tela e tratando execeção

--- aplicacao/mainzada.java
+++ aplicacao/mainzada.java
@@ -1,32 +1,43 @@
 package Xadrez.aplicacao;
 
-
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import Xadrez.xadrezin.excecaoxadrez;
 import Xadrez.xadrezin.partidaxadrez;
 import Xadrez.xadrezin.xadrezpeca;
 import Xadrez.xadrezin.xadrezposicao;
 
-
 public class mainzada {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+       Scanner scan = new Scanner(System.in);
        partidaxadrez partidaxadrez = new partidaxadrez();
        while(true){
-        UI.printartabuleiro(partidaxadrez.getpecas());  
-        System.out.println();
-        System.out.print("Posicao de origem: ");
-        xadrezposicao origem = UI.lerPosicaoXadrez(scan);
-        
-        System.out.println();
-        System.out.print("Posicao de destino: ");
-        xadrezposicao destino = UI.lerPosicaoXadrez(scan);
+        try{
+            UI.limparTelaConsole();   
+            UI.printartabuleiro(partidaxadrez.getpecas());  
+            System.out.println();
+            System.out.print("Posicao de origem: ");
+            xadrezposicao origem = UI.lerPosicaoXadrez(scan);
+            
+            System.out.println();
+            System.out.print("Posicao de destino: ");
+            xadrezposicao destino = UI.lerPosicaoXadrez(scan);
 
-        xadrezpeca pecaCapturada = partidaxadrez.perfomaceMoveXadrezPeca(origem, destino);
-        //System.out.println("pecapturada: " + pecaCapturada);
+            xadrezpeca pecaCapturada = partidaxadrez.perfomaceMoveXadrezPeca(origem, destino);
+            //System.out.println("pecapturada: " + pecaCapturada);
+        }
+        catch(excecaoxadrez e){
+            System.out.println(e.getMessage());
+            scan.nextLine();
+
+        }
+        catch(InputMismatchException e){
+            System.out.println(e.getMessage());
+            scan.nextLine();
+        }
        }
         
     }
-    
+
 }
