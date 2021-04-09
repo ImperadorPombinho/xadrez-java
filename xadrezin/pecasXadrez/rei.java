0edf7b4bf58f528aa349fc830d0b57c186d52f98
ImperadorPombinho <pedrobarros0101@outlook.com>
2021-04-09 19:42:58 -0300

    implementando as movimentações do rei

--- xadrezin/pecasXadrez/rei.java
+++ xadrezin/pecasXadrez/rei.java
@@ -1,5 +1,6 @@
 package Xadrez.xadrezin.pecasXadrez;
 
+import Xadrez.tabuleirogame.posicao;
 import Xadrez.tabuleirogame.tabuleiro;
 import Xadrez.xadrezin.cor;
 import Xadrez.xadrezin.xadrezpeca;
@@ -15,9 +16,56 @@ public class rei extends xadrezpeca{
         //k simboliza rei no tabuleiro
         return "♚";
     }
+    private boolean podeMover(posicao posicao){
+        xadrezpeca aux = (xadrezpeca)getTabul().peca(posicao);
+        return aux == null || aux.getCorzinha() != getCorzinha();
+    }
     @Override
     public boolean[][] possiveisMovimentos() {
         boolean[][] matriz = new boolean[getTabul().getLinhas()][getTabul().getColunas()];
+
+        posicao auxPosicao = new posicao(0, 0);
+        //acima
+        auxPosicao.setCoordenada(posicaoo.getLinha() - 1, posicaoo.getColuna());
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+        //abaixo
+        auxPosicao.setCoordenada(posicaoo.getLinha() + 1, posicaoo.getColuna());
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+        //esquerda
+        auxPosicao.setCoordenada(posicaoo.getLinha(), posicaoo.getColuna() - 1);
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+        //direita
+        auxPosicao.setCoordenada(posicaoo.getLinha(), posicaoo.getColuna() + 1);
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+        //supEsquerda
+        auxPosicao.setCoordenada(posicaoo.getLinha() - 1, posicaoo.getColuna() - 1);
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+        //infEsquerda
+        auxPosicao.setCoordenada(posicaoo.getLinha() + 1, posicaoo.getColuna() - 1);
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+        //supDireita
+        auxPosicao.setCoordenada(posicaoo.getLinha() - 1, posicaoo.getColuna() + 1);
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+        //infDireita
+        auxPosicao.setCoordenada(posicaoo.getLinha() + 1, posicaoo.getColuna() + 1);
+        if(getTabul().posicaoExiste(auxPosicao) && podeMover(auxPosicao)){
+            matriz[auxPosicao.getLinha()][auxPosicao.getColuna()] = true;
+        }
+
         return matriz;
     }
 }
